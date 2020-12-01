0dc5fea60645019879ab8d679297e429436b5815
rik53 <rik53@yandex.ru>
2020-12-02 00:09:17 +0300

    [upd] ДЗ.1

---
+++ lesson_1/ex.1.py
@@ -0,0 +1,9 @@
+product = 'Ручка'
+wheight = 80
+prise = 2.86
+print('Товар: ', product, ' Вес: ', wheight, ' Цена: ', prise )
+
+product = input('Товар: ')
+wheight = int(input('Вес: '))
+prise = float(input('Цена: '))
+print('Товар: ', product, ' Вес: ', wheight, ' Цена: ', prise)
---
+++ lesson_1/ex.3.py
@@ -0,0 +1,5 @@
+number = input('Введите целое положительное число: ')
+n = int(number)
+nn = int(f'{number}{number}')
+nnn = int(f'{number}{number}{number}')
+print(n +nn + nnn)
